dd8b84e5c0702a88164ccd95b177f98af0ba4fcc
Luvy <luveshenp@gmail.com>
2022-12-02 17:19:55 +0200

    aoc: day 1 & day 2

---
+++ 2022/day01/CalorieCounting/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Problem statement: https://adventofcode.com/2022/day/1
+
+var inputString = File.ReadAllText("input.txt");
+Console.WriteLine(TotalCalories(inputString));
+Console.WriteLine(Top3CaloriesSum(inputString));
+
+int TotalCalories(string str)	// Part 1
+{
+    return str.Split(Environment.NewLine)
+        .Aggregate(new List<int> { 0 }, (list, s) =>
+        {
+            if (s == string.Empty)
+            {
+                list.Add(0);
+            }
+            else
+            {
+                list[^1] += int.Parse(s);
+            }
+
+            return list;
+        })
+        .Max();
+}
+
+int Top3CaloriesSum(string str)	// Part 2  
+{
+    return str.Split(Environment.NewLine)
+        .Aggregate(new List<int> { 0 }, (list, s) =>
+        {
+            if (s == string.Empty)
+            {
+                list.Add(0);
+            }
+            else
+            {
+                list[^1] += int.Parse(s);
+            }
+
+            return list;
+        })
+        .OrderBy(i => i)
+        .TakeLast(3)
+        .Sum();
+}
---
+++ 2022/day02/RockPaperScissors/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Problem statement: https://adventofcode.com/2022/day/2
+
+var inputString = File.ReadAllText("example.txt");
+Console.WriteLine(StrategyGuide1(inputString));
+Console.WriteLine(StrategyGuide2(inputString));
+
+int StrategyGuide1(string str)  // Part 1
+{
+    return str.Split(Environment.NewLine)
+        .Select(r => r.Split(' ').Select(s => s[0]).ToArray())
+        .Sum(c =>(c[0] == c[1] - 23 ? 3 : (c[1] - 23 - c[0] + 3) %3 == 1 ? 6 : 0) + (c[1] - 87));
+}
+
+int StrategyGuide2(string str)  // Part 2
+{
+    return str.Split(Environment.NewLine)
+        .Select(r => r.Split(' ').Select(s => s[0]).ToArray())
+        .Sum(c => c[1] == 'Y' ? c[0] - 64 + 3
+            : c[1] == 'Z' ? 6 + ((c[0] - 64 + 1) % 3 > 0 ? (c[0] - 64 + 1) % 3 : 3)
+            : (c[0] - 64 + 2) % 3 > 0 ? (c[0] - 64 + 2) % 3 : 3);
+}
