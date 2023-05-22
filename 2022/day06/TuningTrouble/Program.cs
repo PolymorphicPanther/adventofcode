42fd1b4f7c160d8c9baeb39865507dc810852c6e
Luvy <luveshenp@gmail.com>
2023-05-22 11:48:41 +0200

    aoc: day 6 clean up

--- 2022/day06/TuningTrouble/Program.cs
+++ 2022/day06/TuningTrouble/Program.cs
@@ -4,10 +4,9 @@
 var text = File.ReadAllText("input.txt");
 var markerLen = 14;
 Console.WriteLine(FindMarker(text, markerLen));
-Console.WriteLine(FindMarker2(text, markerLen));
 
 
-int FindMarker2(string str, int markerLen)
+int FindMarker(string str, int markerLen)
 {
     var charMap = new Dictionary<char, int>(markerLen);
     for (var i = 0; i < str.Length; i++)
@@ -35,64 +34,4 @@ int FindMarker2(string str, int markerLen)
     }
 
     throw new Exception("Whoops!");
-}
-
-
-int FindMarker(string str, int markerLen)
-{
-    var charMap = new Dictionary<char, int>();
-    for (var i = 0; i < markerLen; i++)
-    {
-        var ch = str[i];
-        if (charMap.ContainsKey(ch))
-        {
-            charMap[ch]++;
-        }
-        else
-        {
-            charMap[ch] = 1;
-        }
-    }
-    
-    for(var i = markerLen; i < str.Length; i++)
-    {
-
-        var diffChars = 0;
-        if (charMap.Count == markerLen)
-        {
-            foreach (var keyValuePair in charMap)
-            {
-                var res = keyValuePair.Value == 1 ? 1 : 0;
-                diffChars += res;
-            }
-        }
-        
-        
-        if (diffChars == markerLen)
-        {
-            return i;
-        }
-
-        var ch = str[i];
-        if(charMap.ContainsKey(ch))
-        {
-            charMap[ch]++;
-        }
-        else
-        {
-            charMap[ch] = 1;
-        }
-
-        var chToRemove = str[i - markerLen];
-        if (charMap[chToRemove] > 1)
-        {
-            charMap[chToRemove]--;
-        }
-        else
-        {
-            charMap.Remove(chToRemove);
-        }
-
-    }
-    throw new Exception();
 }
